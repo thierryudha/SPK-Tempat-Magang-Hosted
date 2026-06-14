c38e18538cf9f321dc591d01a1fcdfc6d28dea9f
Thierry <thierryud2476@gmail.com>
2026-06-14 18:54:52 +0700

    add trusProxies in bootstrap

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,6 +17,7 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
         $middleware->statefulApi();
         $middleware->throttleApi();
+        $middleware->trustProxies(at: "*");
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
